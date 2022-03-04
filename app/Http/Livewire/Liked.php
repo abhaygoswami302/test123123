11298fa79196e6084f2fa9b5eba63113565d8eb2
kaurlove12345 <lovepreet@nimbletechnocrats.com>
2022-03-04 14:25:01 +0530

    limit changed from 5 to 1000

--- app/Http/Livewire/Liked.php
+++ app/Http/Livewire/Liked.php
@@ -1,74 +1,148 @@
-<?php
-
-namespace App\Http\Livewire;
-
-use App\Models\Like;
-use App\Models\Product;
-use App\Models\User;
-use App\Models\UserCollection;
-use Illuminate\Support\Facades\Auth;
-use Livewire\Component;
-
-class Liked extends Component
-{
-    public $product , $toggle = 10, $flag = 0, $count_exceeded = 0;
-
-    public function mount($product)
-    {
-        $check = UserCollection::where([
-            ['user_id', '=', Auth::user()->id],
-            ['product_id', '=', $product['id']]
-        ])->get();
-        if($check){
-            foreach ($check as $key => $c1) {
-                return $this->flag++;
-            }
-        }
-    }
-
-    public function save($product)
-    {
-            $check = UserCollection::where([
-                ['user_id', '=', Auth::user()->id],
-                ['product_id', '=', $product['id']]
-            ])->first();
-            if ($check) {
-                $bookmark2 = UserCollection::where('id', '=', $check->id)->first();
-                $bookmark2->delete();
-
-                $product = Product::where('id', '=', $product['id'])->first();
-                $product->updated_at = now();
-                $product->save();
-                
-                $this->toggle = 0;
-                return $this->flag = 0;
-            }else{
-
-                $user_membership = User::where('id', '=', Auth::user()->id)->first();
-                if($user_membership->membership == 'Standard'){
-                    $collection_count = UserCollection::where('user_id', '=', Auth::user()->id)->get();
-                    if(count($collection_count) >= 5){
-                        return $this->count_exceeded++;
-                    }
-                }
-
-                $bookmark = new UserCollection();
-                $bookmark->user_id = Auth::user()->id;
-                $bookmark->product_id = $product['id'];
-                $bookmark->save();
-                
-                $product = Product::where('id', '=', $product['id'])->first();
-                $product->updated_at = now();
-                $product->save();
-                $this->toggle = 1;
-
-                return $this->flag++;
-            }
-       
-    }
-
-    public function render()
-    {
-        return view('livewire.liked');
-    }
-}
+<?php
+
+
+
+namespace App\Http\Livewire;
+
+
+
+use App\Models\Like;
+
+use App\Models\Product;
+
+use App\Models\User;
+
+use App\Models\UserCollection;
+
+use Illuminate\Support\Facades\Auth;
+
+use Livewire\Component;
+
+
+
+class Liked extends Component
+
+{
+
+    public $product , $toggle = 10, $flag = 0, $count_exceeded = 0;
+
+
+
+    public function mount($product)
+
+    {
+
+        $check = UserCollection::where([
+
+            ['user_id', '=', Auth::user()->id],
+
+            ['product_id', '=', $product['id']]
+
+        ])->get();
+
+        if($check){
+
+            foreach ($check as $key => $c1) {
+
+                return $this->flag++;
+
+            }
+
+        }
+
+    }
+
+
+
+    public function save($product)
+
+    {
+
+            $check = UserCollection::where([
+
+                ['user_id', '=', Auth::user()->id],
+
+                ['product_id', '=', $product['id']]
+
+            ])->first();
+
+            if ($check) {
+
+                $bookmark2 = UserCollection::where('id', '=', $check->id)->first();
+
+                $bookmark2->delete();
+
+
+
+                $product = Product::where('id', '=', $product['id'])->first();
+
+                $product->updated_at = now();
+
+                $product->save();
+
+                
+
+                $this->toggle = 0;
+
+                return $this->flag = 0;
+
+            }else{
+
+
+
+                $user_membership = User::where('id', '=', Auth::user()->id)->first();
+
+                if($user_membership->membership == 'Standard'){
+
+                    $collection_count = UserCollection::where('user_id', '=', Auth::user()->id)->get();
+
+                    if(count($collection_count) >= 1000){
+
+                        return $this->count_exceeded++;
+
+                    }
+
+                }
+
+
+
+                $bookmark = new UserCollection();
+
+                $bookmark->user_id = Auth::user()->id;
+
+                $bookmark->product_id = $product['id'];
+
+                $bookmark->save();
+
+                
+
+                $product = Product::where('id', '=', $product['id'])->first();
+
+                $product->updated_at = now();
+
+                $product->save();
+
+                $this->toggle = 1;
+
+
+
+                return $this->flag++;
+
+            }
+
+       
+
+    }
+
+
+
+    public function render()
+
+    {
+
+        return view('livewire.liked');
+
+    }
+
+}
+
--- app/Http/Livewire/UserLegoMiniFigures.php
+++ app/Http/Livewire/UserLegoMiniFigures.php
@@ -1,26 +1,52 @@
-<?php
-
-namespace App\Http\Livewire;
-
-use App\Models\UserCollection;
-use Illuminate\Support\Facades\Auth;
-use Livewire\Component;
-
-class UserLegoMiniFigures extends Component
-{
-    public $count_exceeded = 0;
-
-    public function mount()
-    {
-        if(Auth::user()->membership == 'Standard'){
-            $collection_count = UserCollection::where('user_id', '=', Auth::user()->id)->get();
-            if(count($collection_count) >= 5){
-                return $this->count_exceeded++;
-            }
-        }
-    }
-    public function render()
-    {
-        return view('livewire.user-lego-mini-figures');
-    }
-}
+<?php
+
+
+
+namespace App\Http\Livewire;
+
+
+
+use App\Models\UserCollection;
+
+use Illuminate\Support\Facades\Auth;
+
+use Livewire\Component;
+
+
+
+class UserLegoMiniFigures extends Component
+
+{
+
+    public $count_exceeded = 0;
+
+
+
+    public function mount()
+
+    {
+
+        if(Auth::user()->membership == 'Standard'){
+
+            $collection_count = UserCollection::where('user_id', '=', Auth::user()->id)->get();
+
+            if(count($collection_count) >= 1000){
+
+                return $this->count_exceeded++;
+
+            }
+
+        }
+
+    }
+
+    public function render()
+
+    {
+
+        return view('livewire.user-lego-mini-figures');
+
+    }
+
+}
+
